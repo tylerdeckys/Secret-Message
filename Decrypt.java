ff8dc7dc1a3ee4c9d5936c5ecf50bf6a6dd92d5b
tylerdeckys <61843952+tylerdeckys@users.noreply.github.com>
2020-09-14 11:16:15 -0600

    Create Decrypt.java

---
+++ Decrypt.java
@@ -0,0 +1,38 @@
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 18 - Decrypt
+ */
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Decrypt {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // TODO: open the "encrypted.txt" file
+
+        // TODO: read one line at a time using a loop
+
+
+            // TODO: create an output string
+
+            // TODO: read one character at a time using a 2nd loop
+
+
+                // TODO: if the character is a period, write a space in the output string
+
+                // TODO: else, if the character is a space, write a dash in the output string
+
+                // TODO: else, write the character that is 10 codes less in the output string
+
+            
+
+            // TODO: display the output string
+
+
+        // TODO: close the input file
+    }
+}
